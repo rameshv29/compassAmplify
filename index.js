79fdfecf753b5b9b72d88d776676d29ce6897c7b
Ramesh Kumar Venkatraman <rameshkumar.venkatraman@gmail.com>
2023-02-14 15:30:42 +0000

    updated index.js

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-function submitForm() {
-  const serverip = document.getElementById("serverip").value;
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+import { API } from 'aws-amplify';
 
-  const data = {
-    "serverip": serverip,
-    "username": username,
-    "password": password
+async function submitData(serverip, username, password) {
+  try {
+    const request = {
+      body: {
+        serverip,
+        username,
+        password
+      }
+    };
+    const response = await API.post('https://6kjae2ai40.execute-api.us-east-1.amazonaws.com/default', '/compassAmplify', request);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
   }
-
-  $.ajax({
-    type: "POST",
-    url: "https://6kjae2ai40.execute-api.us-east-1.amazonaws.com/default/compassAmplify",
-    dataType: "json",
-    contentType: "application/json",
-    data: JSON.stringify(data),
-    success: function(response) {
-      console.log(response);
-    },
-    error: function(xhr, status, error) {
-      console.log(error);
-    }
-  });
 }
-
