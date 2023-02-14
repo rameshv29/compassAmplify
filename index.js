0b7310d9f011ba0091b00c2760c65b08c8447b42
Ramesh Kumar Venkatraman <rameshkumar.venkatraman@gmail.com>
2023-02-14 15:36:56 +0000

    updated index.js with fetch

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-import { API } from 'aws-amplify';
+function submitForm() {
+  const serverip = document.getElementById("serverip").value;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-async function submitData(serverip, username, password) {
-  try {
-    const request = {
-      body: {
-        serverip,
-        username,
-        password
-      }
-    };
-    const response = await API.post('https://6kjae2ai40.execute-api.us-east-1.amazonaws.com/default', '/compassAmplify', request);
+  const data = {
+    serverip,
+    username,
+    password
+  };
+
+  fetch("https://6kjae2ai40.execute-api.us-east-1.amazonaws.com/default/compassAmplify", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(response => {
     console.log(response);
-  } catch (error) {
+  })
+  .catch(error => {
     console.error(error);
-  }
+  });
 }
