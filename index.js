ee74c7ff643c7449c4a5ed7191839e9dc2d2f711
EC2 Default User <ec2-user@ip-172-31-83-80.ec2.internal>
2023-02-14 06:57:25 +0000

    baseline commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function submitForm() {
+  const serverip = document.getElementById("serverip").value;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const data = {
+    "serverip": serverip,
+    "username": username,
+    "password": password
+  }
+
+  $.ajax({
+    type: "POST",
+    url: "https://<api-gateway-endpoint>/prod/myLambdaFunction",
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify(data),
+    success: function(response) {
+      console.log(response);
+    },
+    error: function(xhr, status, error) {
+      console.log(error);
+    }
+  });
+}
+
